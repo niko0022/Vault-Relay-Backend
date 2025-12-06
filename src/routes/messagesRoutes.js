bebc829007540fa4ea450b5ac8f3ea3ccdc33e93
niko0022 <nikolozishotniashvili@gmail.com>
2025-12-06 16:39:27 +0400

    Add message Rest api routes

---
+++ src/routes/messagesRoutes.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const passport = require('passport');
+const { body, query } = require('express-validator');
+const messagesController = require('../controllers/messagesController')
+const router = express.Router();
+const auth = passport.authenticate('jwt', { session: false });
+const { param } = require('express-validator');
+
+
+router.get(
+  '/:conversationId/messages',
+  auth,
+  [
+    query('limit').optional().isInt({ min: 1, max: 100 }).toInt(),
+    query('cursor').optional().isString(),
+  ],
+  messagesController.getMessages
+);
+
+
+router.post(
+  '/:conversationId/messages',
+  [
+    param('conversationId').isUUID().withMessage('invalid conversation id'),
+
+    body('content')
+      .optional({ values: 'falsy' })
+      .trim()
+      .isLength({ max: 5000 }).withMessage('content too long (max 5000)')
+      .escape(), 
+    body('contentType')
+      .optional()
+      .isIn(['TEXT', 'MARKDOWN', 'HTML'])
+      .withMessage('invalid content type'),
+    body('attachmentUrl')
+      .optional({ values: 'falsy' })
+      .isURL().withMessage('attachmentUrl must be a valid URL'),
+    body().custom((value, { req }) => {
+      if (!req.body.content && !req.body.attachmentUrl) {
+        throw new Error("either content or attachmentUrl is required");
+      }
+      return true;
+    }),
+  ],
+  messagesController.sendMessage
+);
+
+// mark read
+router.post('/:conversationId/read', auth, messagesController.markRead);
+
+module.exports = router;
